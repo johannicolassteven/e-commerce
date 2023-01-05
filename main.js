11a98fa61293e5787a36a7efbe08d614c4003259
Nicolas Espinosa <nicosteveneg900@gmail.com>
2023-01-05 11:10:51 -0500

    first-commit

---
+++ main.js
@@ -0,0 +1,65 @@
+
+document.querySelector(`.pe`).addEventListener(`mouseover`,()=>{
+    document.querySelector(`.sub`).classList.toggle(`pe-`)
+});
+
+document.querySelector(`.pe`).addEventListener(`mouseout`,()=>{
+    document.querySelector(`.sub`).classList.remove(`pe-`)
+});
+
+document.querySelector(`.entradas`).addEventListener(`mouseover`,()=>{
+    document.querySelector(`.sub.a`).classList.toggle(`entradas-`)
+});
+
+document.querySelector(`.entradas`).addEventListener(`mouseout`,()=>{
+    document.querySelector(`.sub.a`).classList.remove(`entradas-`)
+});
+
+
+document.querySelector(`.store`).addEventListener(`mouseover`,()=>{
+    document.querySelector(`.sub.b`).classList.toggle(`store-`)
+});
+
+document.querySelector(`.store`).addEventListener(`mouseout`,()=>{
+    document.querySelector(`.sub.b`).classList.remove(`store-`)
+});
+
+
+document.querySelector(`.culers`).addEventListener(`mouseover`,()=>{
+    document.querySelector(`.sub.c`).classList.toggle(`culers-`)
+});
+
+document.querySelector(`.culers`).addEventListener(`mouseout`,()=>{
+    document.querySelector(`.sub.c`).classList.remove(`culers-`)
+});
+
+document.querySelector(`.club`).addEventListener(`mouseover`,()=>{
+    document.querySelector(`.sub.d`).classList.toggle(`club-`)
+});
+
+document.querySelector(`.club`).addEventListener(`mouseout`,()=>{
+    document.querySelector(`.sub.d`).classList.remove(`club-`)
+});
+
+
+
+document.querySelector(`.socios`).addEventListener(`mouseover`,()=>{
+    document.querySelector(`.sub.e`).classList.toggle(`socios-`)
+});
+
+document.querySelector(`.socios`).addEventListener(`mouseout`,()=>{
+    document.querySelector(`.sub.e`).classList.remove(`socios-`)
+});
+
+
+document.querySelector(`.more`).addEventListener(`mouseover`,()=>{
+    document.querySelector(`.sub.f`).classList.toggle(`more-`)
+});
+
+document.querySelector(`.more`).addEventListener(`mouseout`,()=>{
+    document.querySelector(`.sub.f`).classList.remove(`more-`)
+});
+
+
+
+
